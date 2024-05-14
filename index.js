72e89c5c0ef3074dd07a1b38bfb0ae645d05bb10
Havva Kuş <havvaakus@gmail.com>
2024-05-14 16:18:58 +0300

    API Key

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ const searchBtn = document.getElementById("search-btn")
 
 searchBtn.addEventListener("click", async() => {
     const searchInput = document.getElementById("search-input").value
-    const apiKey = `55ab83a3`
     
-    const res = await fetch (`http://www.omdbapi.com/?apikey=${apiKey}&s=${searchInput}`)
+    const res = await fetch (`http://www.omdbapi.com/?apikey=55ab83a3&s=${searchInput}`)
     const data = await res.json()
     console.log(data)       
 })
