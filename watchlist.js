a95e3082a8d186c5ab333aeedad82a6b5de49a90
Havva Kuş <havvaakus@gmail.com>
2024-05-16 17:51:33 +0300

    .

--- watchlist.js
+++ watchlist.js
@@ -11,3 +11,19 @@ async function loadMovies(searchTerm){
     console.log(data.Search);
 }
 loadMovies("superman")
+
+async function getMovieDetailsAsync (id){
+    const baseURL = `https://omdbapi.com/?i=${searchTerm}&apikey=55ab83a3`
+        try {
+            // make request 
+            const detailResponse = await fetch(`${baseURL}}`);
+            // get response : json
+            const details = await detailResponse.json();
+            // movie
+            const movie = new Movie(details.imdbID, details.Title, details.imdbRating, details.Runtime, details.Genre, details.Plot, details.Poster);
+            //return movie
+            return movie
+        } catch (detailError) {
+            console.error('Failed to fetch movie details:', detailError);
+        }
+    }
